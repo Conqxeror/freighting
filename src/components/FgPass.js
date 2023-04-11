013128fecbfff7861cd3f9c35644c31128c7ee29
Wali Mohammad Kadri <kadriwalimohammad@gmail.com>
2023-04-12 00:35:48 +0530

    ini commit

---
+++ src/components/FgPass.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FgPass() {
+  return (
+    <div>FgPass</div>
+  )
+}
+
+export default FgPass
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div>Click on Profile for login page</div>
+      <div>Click on Trip For Trip Layout Page</div>
+      <div>Click on search for seacrh Layout</div>
+    </>
+  );
+}
+
+export default Home;
